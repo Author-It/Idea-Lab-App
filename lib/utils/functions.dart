8e951eb5b285a43d83a6745aff82a5a967255cdd
Akshat Jain <akshat00jain@gmail.com>
2023-11-24 15:44:06 +0530

    commit

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,14 +1,18 @@
-Future<String> getRandQuote() async {
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<List> getRandQuote() async {
   // Get the data from the API
-  var response = await http.get('https://zenquotes.io/api/random');
+  var response = await http.get(Uri.parse('https://zenquotes.io/api/random'));
 
   // Convert the data to JSON
-  var data = jsonDecode(response.body);
+  var data = json.decode(response.body);
 
   // Get the quote and author
-  String quote = data['content'];
-  String author = data['author'];
+  String quote = data[0]["c"];
+  String author = data[0]["a"];
 
   // Return the quote and author
-  return '$quote - $author';
+  return [quote, author];
 }
